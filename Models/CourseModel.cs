f46ae427e2e3707bb9c0ef61aef0ec631f396c0b
Paulus <kototensey@gmail.com>
2018-05-24 16:21:27 +0300

    added models and started to realize services

---
+++ Models/CourseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngulatTest.Models
+{
+    public class CourseModel
+    {
+        public int Id { get; set; }
+        public string Specialization { get; set; }
+
+        public GroupModel Group { get; set; }
+    }
+}
