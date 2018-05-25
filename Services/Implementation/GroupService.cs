0b2c764fd3b805a222ff1eb264a3aadd88edf238
Paulus <kototensey@gmail.com>
2018-05-25 11:05:59 +0300

    realized service, only basic functionality

--- Services/Implementation/GroupService.cs
+++ Services/Implementation/GroupService.cs
@@ -1,11 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AngulatTest.Domain.Entities;
+using AngulatTest.Domain.Interfaces;
+using AngulatTest.Models;
+using AngulatTest.Services.Interfaces;
 
 namespace AngulatTest.Services.Implementation
 {
-    public class GroupService
+    public class GroupService : IGroupService
     {
+        private IGroupRepository _repository;
+
+        public GroupService(IGroupRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Add(GroupModel model)
+        {
+            var groupEntity = new GroupEntity
+            {
+                Id = model.Id,
+                Name = model.Name
+            };
+
+            await _repository.AddAsync(groupEntity);
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = await _repository.GetAsync(id);
+            if (entity == null)
+                return;
+            await _repository.DeleteAsync(entity);
+        }
+
+        public async Task<GroupModel> Get(int id)
+        {
+            var entity = await _repository.GetAsync(id);
+
+            var model = new GroupModel
+            {
+                Id = entity.Id,
+                Name = entity.Name
+            };
+
+            return model;
+        }
+
+        public async Task<IEnumerable<GroupModel>> GetAll()
+        {
+            var entities = await _repository.GetAll();
+            var result = entities.Select(entity => new GroupModel
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+            });
+
+            return result;
+        }
+
+        public async Task Update(GroupModel model)
+        {
+            var entity = await _repository.GetAsync(model.Id);
+            await _repository.Update(entity);
+        }
     }
 }
--- Services/Implementation/StudentService.cs
+++ Services/Implementation/StudentService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AngulatTest.Domain.Entities;
+using AngulatTest.Domain.Interfaces;
+using AngulatTest.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,77 @@ namespace AngulatTest.Services.Implementation
 {
     public class StudentService
     {
+        private IStudentRepository _repository;
+
+        public StudentService(IStudentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Add(StudentModel model)
+        {
+            var groupEntity = new GroupEntity
+            {
+                Id = model.Group.Id,
+                Name = model.Group.Name
+            };
+
+            var courseEntity = new StudentEntity
+            {
+                Name = model.Name,
+                GroupId = model.Group.Id,
+                Group = groupEntity
+            };
+            await _repository.AddAsync(courseEntity);
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = await _repository.GetAsync(id);
+            if (entity == null)
+                return;
+            await _repository.DeleteAsync(entity);
+        }
+
+        public async Task<StudentModel> Get(int id)
+        {
+            var entity = await _repository.GetAsync(id);
+
+            var model = new StudentModel
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Group = new GroupModel
+                {
+                    Id = entity.Group.Id,
+                    Name = entity.Group.Name
+                }
+            };
+
+            return model;
+        }
+
+        public async Task<IEnumerable<StudentModel>> GetAll()
+        {
+            var entities = await _repository.GetAll();
+            var result = entities.Select(entity => new StudentModel
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Group = new GroupModel
+                {
+                    Id = entity.Group.Id,
+                    Name = entity.Group.Name
+                }
+            });
+
+            return result;
+        }
+
+        public async Task Update(StudentModel model)
+        {
+            var entity = await _repository.GetAsync(model.Id);
+            await _repository.Update(entity);
+        }
     }
 }
